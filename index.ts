af7eb41595dd4f91537bfb6f37d30ef2df102af4
Jaikumarvanjare <jaikumarvanjare2022@gmail.com>
2026-03-13 13:56:06 +0530

    feat: setup basic express server

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Notification Service Running");
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Notification server running on port ${PORT}`);
+});
